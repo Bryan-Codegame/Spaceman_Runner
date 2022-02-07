dd498543c8a52f5062eb895eeddfb9fb79aef3f7
bryanLaptop <bryanesc26@gmail.com>
2022-02-07 15:54:50 -0600

    Create jump() and config Physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce = 0.6f;
+
+    Rigidbody2D m_Rigidbody;
+
+    public LayerMask groundMask; 
+
+    bool isInAir;
+    private void Awake() {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            
+            if(!isInTheGround()) {
+                jump();
+            }
+           
+        }
     }
+
+    void jump() {
+        m_Rigidbody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+    }
+
+    //Raycast devuelve un booleano
+    bool isInTheGround() {
+        float distanceRay = 1f;
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, distanceRay, groundMask)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
