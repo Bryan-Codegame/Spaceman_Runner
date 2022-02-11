9190a4eace1f2d9dd87169794279e458a58287f9
bryanLaptop <bryanesc26@gmail.com>
2022-02-11 00:40:15 -0600

    Jump finished with animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,53 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    //Animation const string
+    const string ON_THE_GROUND = "isOnTheGround";
+    const string IS_ALIVE = "isAlive";
+
     [SerializeField]
     private float distanceRay = 1f;
     
     public float jumpForce = 0.6f;
 
     Rigidbody2D m_Rigidbody;
+    Animator m_Animator;
 
     public LayerMask groundMask; 
 
     bool isInAir;
 
+
     private void Awake() {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator.SetBool(ON_THE_GROUND, true);
+        m_Animator.SetBool(IS_ALIVE, true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
-        {
-            Debug.Log("Press Space");
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {            
             Jump();  
         }
 
-        
+        m_Animator.SetBool(ON_THE_GROUND, isOnTheGround()); 
         //Debug RayCast
         Debug.DrawRay(this.transform.position, Vector2.down * distanceRay, Color.green);
+        Debug.Log(m_Rigidbody.velocity.y);
     }
 
     void Jump() {
 
         //Si está en el piso va a saltar 
-        if(isInTheGround()) {
+        if(isOnTheGround()) {
             m_Rigidbody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
-        }
-        
+        } 
     }
 
     //Raycast devuelve un booleano
-    bool isInTheGround() {
+    bool isOnTheGround() {
       
         if (Physics2D.Raycast(this.transform.position, Vector2.down, distanceRay, groundMask)) {
+
             return true;
         } else {
             return false;
