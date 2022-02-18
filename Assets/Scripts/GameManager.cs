587474f206be3ec2a47a31a46e3ee00131fe55f9
bryanLaptop <bryanesc26@gmail.com>
2022-02-18 15:21:18 -0600

    Game Manager initialized

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState {
+    menu,
+    inGame,
+    gameOver,
+}
 public class GameManager : MonoBehaviour
 {
+    public GameState currentGameState = GameState.menu;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,33 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartGame() 
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void BackToMenu() 
+    {
+
+    }
+
+    private void SetGameState(GameState state)
+    {
+        if (state == GameState.menu){
+            //TODO: Menu
+        } else if (state == GameState.inGame) {
+            //TODO: inGame
+        } else if (state == GameState.gameOver) {
+            //TODO: gameOver
+        }
+
+        this.currentGameState = state;
     }
 }
